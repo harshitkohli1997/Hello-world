0c9d34d964a922bfa4a8e841ac52c19fda43dacf
harshit kohli <harshit.kohli1997@gmail.com>
2019-10-01 14:54:12 +0530

    Update catalan.cpp

--- C++/catalan.cpp
+++ C++/catalan.cpp
@@ -1,1 +1,20 @@
 
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+int n;
+cin>>n;
+  int a[n];
+  a[0] = 1;
+  a[1]= 1;
+ for (int i=2; i<=n; i++) 
+    { 
+        a[i] = 0; 
+        for (int j=0; j<i; j++) 
+            a[i] += a[j] * a[i-j-1]; 
+    }
+  cout<<a[n];
+}
